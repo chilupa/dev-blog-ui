9b0872bb50572e20eb9fd8000f37164b3867d9dd
Pavan Chilukuri <pavan.chilukuri@dhcs.ca.gov>
2021-02-27 14:27:47 -0800

    Add reusabe card component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import { posts } from '../../mocks/posts';
 import Posts from './Posts';
 import Promotion from './Promotion';
 import Menu from './Menu';
+import SimpleCard from '../../components/SimpleCard/SimpleCard';
 
 const Home = () => {
   return (
@@ -21,6 +22,18 @@ const Home = () => {
         </Grid>
         <Grid item xs={3}>
           <Promotion title="Promotion 1" description="Promotion description" />
+          <Box>
+            <SimpleCard
+              title="Join Global Change Hackathon"
+              description="Some description about Hackathon which will help user know what the Hackathon is about"
+            />
+          </Box>
+          <Box pt={1}>
+            <SimpleCard
+              title="Join Global Change Hackathon"
+              description="Some description about Hackathon which will help user know what the Hackathon is about"
+            />
+          </Box>
         </Grid>
       </Grid>
     </Box>
