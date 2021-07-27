b94ea4fd385581350dfc2c4c40fe086fef6b82c0
chilupa <ichilupa@gmail.com>
2021-07-26 21:22:05 -0700

    Add coverage report and breakpoint detection

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -9,15 +9,13 @@ import { theme } from '../../utils';
 import Header from '../../components/Header/Header';
 import Home from '../Home/Home';
 
-function App() {
-  return (
+const App = () =>
     <ThemeProvider theme={responsiveFontSizes(theme)}>
       <Container maxWidth="lg">
         <Header />
         <Home />
       </Container>
     </ThemeProvider>
-  );
-}
+  
 
 export default App;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Box, Grid } from '@material-ui/core';
+import { Box, Grid, useMediaQuery } from '@material-ui/core';
 import { posts } from '../../mocks/posts';
 import Posts from './Posts';
 import Promotion from './Promotion';
-import Menu from './Menu';
+import Menu from '../../components/Menu/Menu';
 
 const Home = () => {
+  const isMobileOrTabletDevice = useMediaQuery(
+    (theme) => theme.breakpoints.up('sm') || theme.breakpoints.up('md')
+  );
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid item xs={3}>
-          <Box>
-            <Menu />
-          </Box>
-          <Box pt={1}>Tags</Box>
-          <Box pt={1}>Some Ad</Box>
+        <Grid item xs={12} sm={3} md={3} lg={3}>
+          {isMobileOrTabletDevice && <Menu />}
+          {isMobileOrTabletDevice && <Box pt={1}>Tags</Box>}
+          {isMobileOrTabletDevice && <Box pt={1}>Some Ad</Box>}
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12} sm={6} md={6} lg={6}>
           <Posts posts={posts} />
         </Grid>
-        <Grid item xs={3}>
+        <Grid item xs={12} sm={3} md={3} lg={3}>
           <Promotion title="Promotion 1" description="Promotion description" />
+          <Box pt={2}>
+            <Promotion
+              title="Promotion 2"
+              description="Promotion description"
+            />
+          </Box>
         </Grid>
       </Grid>
     </Box>
