7c282c4c98982f27eb992087d0d528683636f30f
chilupa <ichilupa@gmail.com>
2021-07-27 13:40:34 -0700

    Add posts to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Box, Grid, useMediaQuery } from '@material-ui/core';
 import { posts } from '../../mocks/posts';
-import Posts from './Posts';
 import Promotion from './Promotion';
 import Menu from '../../components/Menu/Menu';
+import PostCard from '../../components/PostCard/PostCard';
 
 const Home = () => {
   const isMobileOrTabletDevice = useMediaQuery(
@@ -18,7 +18,9 @@ const Home = () => {
           {isMobileOrTabletDevice && <Box pt={1}>Some Ad</Box>}
         </Grid>
         <Grid item xs={12} sm={6} md={6} lg={6}>
-          <Posts posts={posts} />
+          {posts.map((post, index) => (
+            <PostCard key={index} {...post} />
+          ))}
         </Grid>
         <Grid item xs={12} sm={3} md={3} lg={3}>
           <Promotion title="Promotion 1" description="Promotion description" />
